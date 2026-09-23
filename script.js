d27a60f9af2c3de39ce9bd1e0d8b092366906e7d
anablipblip <anais.morales6bis@gmail.com>
2026-09-23 15:43:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// L'Hôtel des Mers Veilleuses s'éveillera ici.
+gsap.to(".wave1",{
+  y:-4,
+  duration:8,
+  repeat:-1,
+  yoyo:true,
+  ease:"sine.inOut"
+});
+
+gsap.to(".wave2",{
+  y:3,
+  duration:10,
+  repeat:-1,
+  yoyo:true,
+  ease:"sine.inOut"
+});
