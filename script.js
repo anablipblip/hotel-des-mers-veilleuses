694cdcbf864c5ca66bdc5f838c399deddbadf4c0
anablipblip <anais.morales6bis@gmail.com>
2026-09-23 16:15:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ gsap.to(".wave2",{
   yoyo:true,
   ease:"sine.inOut"
 });
+/* Veilleuse */
+
+const veilleuse=document.querySelector(".veilleuse");
+
+if(window.matchMedia("(hover:hover)").matches){
+
+  window.addEventListener("mousemove",(e)=>{
+
+    gsap.to(veilleuse,{
+      x:e.clientX,
+      y:e.clientY,
+      duration:.18,
+      ease:"power2.out"
+    });
+
+    veilleuse.style.opacity=1;
+  });
+
+  window.addEventListener("mouseleave",()=>{
+    veilleuse.style.opacity=0;
+  });
+
+}
