b168698079ca895973750d5f79e8fa76ca22b665
anablipblip <anais.morales6bis@gmail.com>
2026-09-23 16:43:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
+// Respiration des vagues
+
 gsap.to(".wave1",{
-  y:-4,
+
+  y:-3,
+
   duration:8,
+
   repeat:-1,
+
   yoyo:true,
+
   ease:"sine.inOut"
+
 });
 
 gsap.to(".wave2",{
-  y:3,
+
+  y:4,
+
   duration:10,
+
   repeat:-1,
+
   yoyo:true,
+
   ease:"sine.inOut"
+
 });
-/* Veilleuse */
-
-const veilleuse=document.querySelector(".veilleuse");
-
-if(window.matchMedia("(hover:hover)").matches){
-
-  window.addEventListener("mousemove",(e)=>{
-
-    gsap.to(veilleuse,{
-      x:e.clientX,
-      y:e.clientY,
-      duration:.18,
-      ease:"power2.out"
-    });
-
-    veilleuse.style.opacity=1;
-  });
-
-  window.addEventListener("mouseleave",()=>{
-    veilleuse.style.opacity=0;
-  });
-
 }
